72a6b5fc25d45a8a5e8c4629fd12cb9ddb695374
Vdot Babyy <130109175+VdotBabyy@users.noreply.github.com>
2023-09-06 23:09:13 -0400

    Update .lua

--- .lua
+++ .lua
@@ -1,41 +1,35 @@
-# Compiled Lua sources
-luac.out
+local function onCharacterAdded(character)
+    wait(1) -- Wait for the character to fully load
+    
+    local head = character:FindFirstChild("Head")
+    if head then
+        head.MeshId = "http://www.roblox.com/asset/?id=6686307858"
+        head.TextureID = "http://www.roblox.com/asset/?id=6686307858"
+        head.Transparency = 1
+    end
 
-# luarocks build files
-*.src.rock
-*.zip
-*.tar.gz
+    local rightLowerLeg = character:FindFirstChild("RightLowerLeg")
+    if rightLowerLeg then
+        rightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+        rightLowerLeg.Transparency = 1
+    end
 
-# Object files
-*.o
-*.os
-*.ko
-*.obj
-*.elf
+    local rightUpperLeg = character:FindFirstChild("RightUpperLeg")
+    if rightUpperLeg then
+        rightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+        rightUpperLeg.TextureID = "http://www.roblox.com/asset/?id=902843398"
+    end
 
-# Precompiled Headers
-*.gch
-*.pch
+    local rightFoot = character:FindFirstChild("RightFoot")
+    if rightFoot then
+        rightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
+        rightFoot.Transparency = 1
+    end
+end
 
-# Libraries
-*.lib
-*.a
-*.la
-*.lo
-*.def
-*.exp
+game.Players.LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
 
-# Shared objects (inc. Windows DLLs)
-*.dll
-*.so
-*.so.*
-*.dylib
-
-# Executables
-*.exe
-*.out
-*.app
-*.i*86
-*.x86_64
-*.hex
-
+-- In case the character is already spawned when the script runs
+if game.Players.LocalPlayer.Character then
+    onCharacterAdded(game.Players.LocalPlayer.Character)
+end
